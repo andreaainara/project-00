3aee7ec748d71785927fd131b44b24a676eecbf7
andreaainara <andrea.ainara10@gmail.com>
2016-03-26 21:07:03 -0700

    html debugging, more javascript

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ $(document).on('ready', function() {
   console.log('JS is loaded!');
 
   $(window).on("keypress", function moveRight(e) {
-    if (e.keycode === 97) {
+    if (e.keyCode === 97) {
       $('#player-one').animate({'left': '+=10px' }, 0);
-    } else if (e.keycode === 108) {
+    } else if (e.keyCode === 108) {
       $('#player-two').animate({ 'left': '+=10px' }, 0);
     }
   });
