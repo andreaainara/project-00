8dbd4a138673ac6315a3406a2aa1fa0120361af6
andreaainara <andrea.ainara10@gmail.com>
2016-03-26 20:59:22 -0700

    javascript setting up event listener

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 $(document).on('ready', function() {
 
   console.log('JS is loaded!');
+
+  $(window).on("keypress", function moveRight(e) {
+    if (e.keycode === 97) {
+      $('#player-one').animate({'left': '+=10px' }, 0);
+    } else if (e.keycode === 108) {
+      $('#player-two').animate({ 'left': '+=10px' }, 0);
+    }
+  });
 });
