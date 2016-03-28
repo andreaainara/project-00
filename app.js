6a9bf4fa5e6faad09478bd39328381e4243a83f3
andreaainara <andrea.ainara10@gmail.com>
2016-03-27 17:20:46 -0700

    more debugging, reset button fixes

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ $(document).on('ready', function() {
 
   console.log('JS is loaded!');
 
-  $(window).on("keypress", function moveRight(e) {
+  var playerOne = $('#player-one');
+  var playerTwo = $('#player-two');
+
+  $(window).on("keypress", function readySetGo(e) {
     if (e.keyCode === 97) {
-      $('#player-one').animate({'left': '+=10px' }, 0);
+      playerOne.animate({'left': '+=10px' }, 0);
     } else if (e.keyCode === 108) {
-      $('#player-two').animate({ 'left': '+=10px' }, 0);
+      playerTwo.animate({ 'left': '+=10px' }, 0);
     }
+  });
+
+  function getWinner() {
+    if(playerOne.css('left') == '810px'){
+        alert("player one wins!");
+  } else if (playerTwo.css('left') =='810px'){
+        alert("player two wins!");
+  }
+}
+  $('#reset').on("click" , function resetGame(e){
+    playerOne.animate ({
+      'left': '0px'
+    });
+    playerTwo.animate({
+      'left': '0px'
+    } ,0 );
   });
 });
